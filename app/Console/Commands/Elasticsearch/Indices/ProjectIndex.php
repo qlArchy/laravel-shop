2a9e4ae6e62694873a41a86c9b569e27bd4aa3e8
vagrant <vagrant@homestead>
2019-07-15 06:01:38 +0000

    Elasticsearch 索引迁移命令

--- app/Console/Commands/Elasticsearch/Indices/ProjectIndex.php
+++ app/Console/Commands/Elasticsearch/Indices/ProjectIndex.php
@@ -1,7 +1,6 @@
 <?php
 namespace App\Console\Commands\Elasticsearch\Indices;
 use Illuminate\Support\Facades\Artisan;
-
 class ProjectIndex
 {
     public static function getAliasName()
@@ -9,43 +8,49 @@ class ProjectIndex
         return 'products';
     }
 
-    public static function getProperties()
+  public static function getProperties()
     {
         return [
-            'type'          => ['type' => 'keyword'],
-            'title'         => ['type' => 'text', 'analyzer' => 'ik_smart', 'search_analyzer' => 'ik_smart_synonym'],
-            'long_title'    => ['type' => 'text', 'analyzer' => 'ik_smart', 'search_analyzer' => 'ik_smart_synonym'],
-            'category_id'   => ['type' => 'integer'],
-            'category'      => ['type' => 'keyword'],
+            'type' => ['type' => 'keyword'],
+            'title' => ['type' => 'text'],
+            'long_title' => ['type' => 'text'],
+
+            // 应该是这样的 但是我没有配置 同义词映射
+            // 'title' => ['type' => 'text', 'analyzer' => 'ik_smart', 'search_analyzer' => 'ik_smart_synonym'],
+            // 'long_title' => ['type' => 'text', 'analyzer' => 'ik_smart', 'search_analyzer' => 'ik_smart_synonym'],
+            'category_id' => ['type' => 'integer'],
+            'category' => ['type' => 'keyword'],
             'category_path' => ['type' => 'keyword'],
-            'description'   => ['type' => 'text', 'analyzer' => 'ik_smart'],
-            'price'         => ['type' => 'scaled_float', 'scaling_factor' => 100],
-            'on_sale'       => ['type' => 'boolean'],
-            'rating'        => ['type' => 'float'],
-            'sold_count'    => ['type' => 'integer'],
-            'review_count'  => ['type' => 'integer'],
-            'skus'          => [
-                'type'       => 'nested',
+            'description' => ['type' => 'text', 'analyzer' => 'ik_smart'],
+            'price' => ['type' => 'scaled_float', 'scaling_factor' => 100],
+            'on_sale' => ['type' => 'boolean'],
+            'rating' => ['type' => 'float'],
+            'sold_count'   => ['type' => 'integer'],
+            'review_count' => ['type' => 'integer'],
+            'skus' => [
+                'type' => 'nested',
                 'properties' => [
-                    'title'       => [
-                        'type'            => 'text',
-                        'analyzer'        => 'ik_smart',
-                        'search_analyzer' => 'ik_smart_synonym',
-                        'copy_to'         => 'skus_title',
+                    'title' => [
+                        'type' => 'text',
+                        // 我没有配置 同义词映射
+                        // 'analyzer' => 'ik_smart',
+                        // 'search_analyzer' => 'ik_smart_synonym',
+                        'copy_to' => 'skus_title',
                     ],
                     'description' => [
-                        'type'     => 'text',
-                        'analyzer' => 'ik_smart',
-                        'copy_to'  => 'skus_description',
+                        'type' => 'text',
+                        // 我没有配置 同义词映射
+                        // 'analyzer' => 'ik_smart',
+                        'copy_to' => 'skus_description',
                     ],
-                    'price'       => ['type' => 'scaled_float', 'scaling_factor' => 100],
+                    'price' => ['type' => 'scaled_float', 'scaling_factor' => 100],
                 ],
             ],
-            'properties'    => [
-                'type'       => 'nested',
+            'properties' => [
+                'type' => 'nested',
                 'properties' => [
-                    'name'         => ['type' => 'keyword'],
-                    'value'        => ['type' => 'keyword', 'copy_to' => 'properties_value'],
+                    'name' => ['type' => 'keyword'],
+                    'value' => ['type' => 'keyword', 'copy_to' => 'properties_value'],
                     'search_value' => ['type' => 'keyword'],
                 ],
             ],
--- app/Console/Commands/Elasticsearch/SyncProducts.php
+++ app/Console/Commands/Elasticsearch/SyncProducts.php
@@ -1,42 +1,36 @@
 <?php
 namespace App\Console\Commands\Elasticsearch;
-
 use App\Models\Product;
 use Illuminate\Console\Command;
-
 class SyncProducts extends Command
 {
-    protected $signature = 'es:sync-products';
-
+    // 添加一个名为 index，默认值为 products 的参数
+    protected $signature = 'es:sync-products {--index=products}';
     protected $description = '将商品数据同步到 Elasticsearch';
-
     public function __construct()
     {
         parent::__construct();
     }
-
     public function handle()
     {
         // 获取 Elasticsearch 对象
         $es = app('es');
-
         Product::query()
             // 预加载 SKU 和 商品属性数据，避免 N + 1 问题
             ->with(['skus', 'properties'])
             // 使用 chunkById 避免一次性加载过多数据
             ->chunkById(100, function ($products) use ($es) {
                 $this->info(sprintf('正在同步 ID 范围为 %s 至 %s 的商品', $products->first()->id, $products->last()->id));
-
                 // 初始化请求体
                 $req = ['body' => []];
                 // 遍历商品
                 foreach ($products as $product) {
                     // 将商品模型转为 Elasticsearch 所用的数组
                     $data = $product->toESArray();
-
                     $req['body'][] = [
                         'index' => [
-                            '_index' => 'products',
+                            // 从参数中读取索引名称
+                            '_index' => $this->option('index'),
                             '_type'  => '_doc',
                             '_id'    => $data['id'],
                         ],
